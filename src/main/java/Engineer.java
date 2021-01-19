d2773ec5ba6cf17a1410ce942f374442778dd903
NNJAMBI <nkaranja7@gmail.com>
2021-01-19 23:11:25 +0300

    Add the the first test and constructor for Engineer

--- src/main/java/Engineer.java
+++ src/main/java/Engineer.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Engineer {
+    private String firstname,lastname,email;
+    private int id;
+
+    public Engineer(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
 }
--- src/test/java/EngineerTest.java
+++ src/test/java/EngineerTest.java
@@ -1,2 +1,16 @@
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class EngineerTest {
+
+    @Test
+    public void newEngineer_instatiatesCorrectly() {
+        Engineer engineer = new Engineer("Nancy", "Karanja", "nkaranja7@gmail.com");
+        assertTrue(engineer instanceof Engineer);
+    }
 }
+
