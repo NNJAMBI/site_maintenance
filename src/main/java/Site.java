b47787432e822afc89d5f6a81ff8af71c6beb4b2
NNJAMBI <nkaranja7@gmail.com>
2021-01-23 00:01:22 +0300

    initiate the sites class, variables and its tests

--- src/main/java/Site.java
+++ src/main/java/Site.java
@@ -1,2 +1,21 @@
+import java.time.LocalDateTime;
+import java.util.List;
+import org.sql2o.*;
+
 public class Site {
+    private String site_name;
+    private LocalDateTime CreatedAt;
+    private int id;
+    private int engineerid;
+
+    public Site(String site_name, int engineerid) {
+        this.site_name = site_name;
+        CreatedAt = LocalDateTime.now();
+        this.engineerid = engineerid;
+    }
+
+    public String getSiteName() {
+        return site_name;
+    }
 }
+
--- src/test/java/SiteTest.java
+++ src/test/java/SiteTest.java
@@ -1,2 +1,28 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class SiteTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void Site_instantiatesCorrectly_true() {
+        Site mySite = new Site("Thika Servers",  1);
+        assertEquals(true, mySite instanceof Site);
+    }
+
+    @Test
+    public void Site_instantiatesWithName_String() {
+        Site mySite = new Site("Thika Servers", 1);
+        assertEquals("Thika Servers", mySite.getSiteName());
+    }
+
+
 }
+
